6c8205629a2813322472168d3c50a8a9330f9b5a
Davor25793 <roko257993@gmail.com>
2021-03-22 22:16:58 +0100

    changes

--- src/app.js
+++ src/app.js
@@ -1,3 +1,11 @@
-const greeting = 'Hello World';
-console.log(greeting);
+const cart = document.querySelector('.cart-container')
+const sidebar = document.querySelector('.sidebar')
+const close = document.querySelector('.close-cart')
 
+cart.addEventListener('click', () => {
+  sidebar.classList.add('show')
+})
+
+close.addEventListener('click', () => {
+  sidebar.classList.remove('show')
+})

093f6f2fe44686e42c46b9633149f51dab11e176
Davor25793 <roko257993@gmail.com>
2021-03-23 21:45:06 +0100

    changes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
+//Variables
 const cart = document.querySelector('.cart-container')
 const sidebar = document.querySelector('.sidebar')
 const close = document.querySelector('.close-cart')
+const featuredContainer = document.querySelector('.featured-container')
 
 cart.addEventListener('click', () => {
   sidebar.classList.add('show')
@@ -9,3 +11,40 @@ cart.addEventListener('click', () => {
 close.addEventListener('click', () => {
   sidebar.classList.remove('show')
 })
+
+
+//Init
+function init(){
+  fetch('db.json')
+    .then(data => data.json())
+    .then(res => displayFeatured(res.items))
+}
+
+//Featured
+const displayFeatured = (items) => {
+  let output = ''
+  console.log(items)
+  const filt = items.filter(item => item.featured)
+  filt.map(item => {
+    output += `
+    <div class="featured-item">
+    <img src=${item.image} alt="">
+    <div class="items">
+      <span class="search">
+        <a href="product.html">
+         <i class="fas fa-search"></i>
+        </a>
+      </span>
+      <span class="add-to-cart">
+       <i class="fas fa-shopping-cart"></i>
+      </span>
+    </div>
+    <div class="name">${item.name}</div>
+    <div class="price">${item.price}</div>
+  </div>
+    `
+  })
+  featuredContainer.innerHTML = output;
+}
+
+init();
